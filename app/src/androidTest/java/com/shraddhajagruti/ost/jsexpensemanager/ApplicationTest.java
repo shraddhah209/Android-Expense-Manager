b9e565311c9a01ddc3d67450321b39869b44ec80
shraddhah209 <shraddhaholmukhe96@gmail.com>
2016-09-30 17:55:37 +0530

    navigator menu

--- app/src/androidTest/java/com/shraddhajagruti/ost/jsexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/com/shraddhajagruti/ost/jsexpensemanager/ApplicationTest.java
@@ -3,11 +3,22 @@ package com.shraddhajagruti.ost.jsexpensemanager;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
+
         super(Application.class);
+
+
     }
+
+   /* @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        // other setup code
+    } */
 }
